6a0fa41b3f2d84771228275ca56709f8a352aa7b
mubarizmayilzade <mubarizbm@code.edu.az>
2022-05-13 21:50:37 +0400

    project 90% done

---
+++ next-news/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Toolbar } from '../components/toolbar'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className='page-container'>
+      <Toolbar/>
+      
+      <div className={styles.main}>
+
+
+          <h1>
+              Next.js News App
+          </h1>
+
+          <h3>
+            Your one stop shop for the latest news article
+          </h3>
+      </div>
+    </div>
+  )
+}
